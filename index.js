6b8d323d8f7c640e189961a349ca5f2939b5fe5a
samixdev <samer.hd.ah@gmail.com>
2021-01-16 14:25:42 +0200

    created timer and btn

---
+++ index.js
@@ -0,0 +1,9 @@
+const sampleTxt = document.getElementById("sample");
+const inputTxt = document.getElementById("textInput");
+const resetBtn = document.getElementById("resetBtn");
+const timerTxt = document.getElementById("timer");
+
+//inputTxt.addEventListener("keypress", start, false);
+//inputTxt.addEventListener("keyup", spellCheck, false);
+//resetBtn.addEventListener("click", reset, false);
+

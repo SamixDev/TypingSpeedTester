4359bf2f7cb1ee8a63a8bef2b05604145ded7d9d
samixdev <samer.hd.ah@gmail.com>
2021-01-16 15:15:39 +0200

    spell check and change border color

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ var interval;
 var timerRunning = false;
 
 inputTxt.addEventListener("keypress", start, false);
-//inputTxt.addEventListener("keyup", spellCheck, false);
+inputTxt.addEventListener("keyup", spellCheck, false);
 resetBtn.addEventListener("click", reset, false);
-
-
-
 
 // Start the timer:
 function start() {
@@ -29,7 +26,6 @@ function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     timerTxt.innerHTML = currentTime;
     timer[3]++;
-
     timer[0] = Math.floor((timer[3]/100)/60);
     timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
@@ -51,4 +47,23 @@ function reset() {
     timerRunning = false;
     inputTxt.value = "";
     timerTxt.innerHTML = "00:00:00";
+    inputTxt.style.borderColor = "#777777";
+}
+
+// Match the input with sample text
+function spellCheck() {
+    let textEntered = inputTxt.value;
+    let originTextMatch = sampleTxt.substring(0,textEntered.length);
+
+    if (textEntered == sampleTxt) {
+        clearInterval(interval);
+        inputTxt.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) {
+            inputTxt.style.borderColor = "#65CCf3";
+        } else {
+            inputTxt.style.borderColor = "#fe4a49";
+        }
+    }
+
 }

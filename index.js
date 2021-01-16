242bb218b86d9b4605c5c4f7179d99c17236bd57
samixdev <samer.hd.ah@gmail.com>
2021-01-16 14:52:45 +0200

    set up timer on start typing

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-const sampleTxt = document.getElementById("sample");
+const sampleTxt = document.getElementById("sample").innerHTML;
 const inputTxt = document.getElementById("textInput");
 const resetBtn = document.getElementById("resetBtn");
 const timerTxt = document.getElementById("timer");
 
-//inputTxt.addEventListener("keypress", start, false);
+var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
+
+inputTxt.addEventListener("keypress", start, false);
 //inputTxt.addEventListener("keyup", spellCheck, false);
 //resetBtn.addEventListener("click", reset, false);
 
+
+
+
+// Start the timer:
+function start() {
+    let textEnterdLength = inputTxt.value.length;
+    if (textEnterdLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
+    }
+    console.log(textEnterdLength);
+}
+
+// Run html timer:
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    timerTxt.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
+
+// Add leading zero
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
